e0d19dd0d31d2671649e38eaf9605a270c10e0ab
Nicolas Valcarcel <nvalcarcel@gmail.com>
2019-07-19 11:42:23 -0500

    use Text for description

--- marketplace/models.py
+++ marketplace/models.py
@@ -5,4 +5,4 @@ class Listing(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
-    description = db.Column(db.String(500))
+    description = db.Column(db.Text())
